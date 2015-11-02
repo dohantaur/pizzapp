dec237f88abe71bb2bcfb14b4fbf8aa38a8df5ab
Aurelien Dohant <dohant.aurelien@gmail.com>
2015-11-02 10:14:45 +0100

    plop

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+var bodyParser = require('body-parser');
+app.set('view engine', 'jade');
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  request('http://pizzapi.herokuapp.com', (err, result, body ) => {
+    if(err) return res.send(err);
+    console.log(body);
+    res.send(body)
+  });
+});
+
+app.get('/pizzas', (req, res) => {
+  request('http://pizzapi.herokuapp.com/pizzas', (err, result, body ) => {
+    if(err) return res.send(err);
+    console.log(body);
+    res.send(body)
+  });
+});
+
+app.get('/orders/:id', (req, res) => {
+  request(`http://pizzapi.herokuapp.com/orders/${req.params.id}`, (err, result, body ) => {
+    if(err) return res.send(err);
+    console.log(body);
+    res.send(body)
+  });
+});
+
+app.post('/orders', (req, res) => {
+  request.post('http://pizzapi.herokuapp.com/orders', req.body ,(err, result, body ) => {
+    if(err) return res.send(err);
+    console.log(body);
+    res.send(body)
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000...')
+})
