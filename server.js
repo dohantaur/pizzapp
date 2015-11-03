5ccae07304995f8c0ed74a8635d3b065eede8d0f
Aurelien Dohant <dohant.aurelien@gmail.com>
2015-11-03 14:09:05 +0100

    plop

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ var bodyParser = require('body-parser');
 var redis = require('redis');
 var favicon = require('serve-favicon');
 var path = require('path');
+var librato = require('librato-node');
 var CircuitBreaker = require('circuit-breaker-js');
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
+app.use(librato.middleware());
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -24,27 +27,28 @@ var breaker = new CircuitBreaker({
     volumeThreshold: 2
 });
 
-breaker.onCircuitOpen = function(metrics) {
-  console.log('CircuitBreaker: Ouvert ! ', metrics);
-  isEnMaintenance = true;
-};
+librato.configure({email: 'julian.licette@gmail.com', token: '0956806394fdc9b6a1314500b6d64a15c3623747de80ef21ade0a91a3d599c5d'});
+librato.start();
 
-breaker.onCircuitClose = function(metrics) {
-  console.warn('CircuitBreaker: Fermé ! ', metrics);
-  isEnMaintenance = false;
-};
+/***************************************************************/
 
 app.get('/', (req, res) => {
+    librato.increment('GET /');
     res.render('index');
 });
 
 app.get('/pizzas', (req, res) => {
+<<<<<<< HEAD
   console.log('PLOP');
   console.log(breaker.isOpen());
+=======
+    librato.increment('GET /pizzas');
+>>>>>>> 35367d1a4a1ca60e0f26bb1e5efcb4712d5693a4
 	getPizzasFromCache(res);
 });
 
 app.get('/orders/:id', (req, res) => {
+    librato.increment('GET /orders/:id');
     request.get({url: `http://pizzapi.herokuapp.com/orders/${req.params.id}`, timeout: 4000}, (err, result, body ) => {
         if(err) {
           if(err.code === 'ETIMEDOUT') {
@@ -58,12 +62,13 @@ app.get('/orders/:id', (req, res) => {
 });
 
 app.post('/orders', (req, res) => {
+    librato.increment('POST /orders');
     function command(success,failed){
         request.post({url: 'http://pizzapi.herokuapp.com/orders', timeout: 4000, body: JSON.stringify({id: parseInt(req.body.id)})}, (err, result, body ) => {
           if(err || result.statusCode == 503){
               console.log('CircuitBreaker: failed');
               failed();
-              res.render('503');
+              redirect503(req,res);
               return;
           }
           console.log('CircuitBreaker: success');
@@ -83,7 +88,7 @@ app.use(function(req, res, next){
   res.status(404);
   // respond with html page
   if (req.accepts('html')) {
-    res.render('404', { url: req.url });
+    redirect404(req,res);
     return;
   }
   // respond with json
@@ -95,10 +100,15 @@ app.use(function(req, res, next){
   res.type('txt').send('Not found');
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000...')
-})
+function redirect404(req,res){
+    librato.increment('statusCode.404');
+    res.render('404', { url: req.url });
+}
 
+function redirect503(req,res){
+    librato.increment('statusCode.503');
+    res.render('503');
+}
 function getPizzasFromCache(res) {
     // var timeout = setTimeout( () => {
     // }, 1000);
@@ -114,3 +124,25 @@ function getPizzasFromCache(res) {
         res.render('pizzas-get', {pizzas: pizzas, isEnMaintenance: breaker.isOpen()});
     });
 }
+
+/*******************************************************************/
+app.listen(3000, () => {
+  console.log('Listening on port 3000...')
+})
+
+var openCount = 0, closedCount = 0;
+breaker.onCircuitOpen = function(metrics) {
+  console.log('CircuitBreaker: Ouvert ! ', metrics);
+  isEnMaintenance = true;
+  librato.increment('circuitBreakerIsOpen');
+};
+
+breaker.onCircuitClose = function(metrics) {
+  console.log('CircuitBreaker: Fermé ! ', metrics);
+  isEnMaintenance = false;
+  librato.increment('circuitBreakerIsClosed');
+};
+
+process.once('SIGINT', function() {
+  librato.stop(); // stop optionally takes a callback
+});
