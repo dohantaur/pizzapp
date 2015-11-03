a3a230384b4dd293d29f85a591e9706b5cf34ea4
Julian <julian.licette@gmail.com>
2015-11-03 17:26:57 +0100

    port express

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ function getPizzasFromCache(res) {
 }
 
 /*******************************************************************/
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port 3000...')
 })
 
